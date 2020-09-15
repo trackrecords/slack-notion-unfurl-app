6b0d506a0a1f09b3223e224d140edaf32677bf27
Masayuki Uehara <munky69rock@gmail.com>
2020-09-15 14:59:04 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,81 @@
+import { App, KnownBlock } from "@slack/bolt";
+import Notion from "notion-api-js";
+import { toUUID } from "to-uuid";
+
+const notionToken: string = process.env.NOTION_TOKEN!;
+const botToken: string = process.env.SLACK_BOT_TOKEN!;
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: botToken,
+});
+
+const notion = new Notion({
+  token: notionToken,
+  options: {},
+});
+
+const extractPageIdFromUrl = (url: string) =>
+  toUUID(url.match(/notion\.so\/trackrecords\/(?:.+\-)?([0-9a-f]+)/)[1]);
+
+// https://api.slack.com/reference/messaging/link-unfurling#slack_app_unfurling
+app.event("link_shared", async ({ event }) => {
+  console.dir(event);
+  const unfurls: { [url: string]: { blocks: KnownBlock[] } } = {};
+
+  await Promise.all(
+    event.links
+      .filter(({ url }) => /notion\.so\/trackrecords\//.test(url))
+      .map(async ({ url }) => {
+        const id = extractPageIdFromUrl(url);
+        const page = await notion.getPageById(id);
+        if (!page.Attributes) return;
+        const { title, teaser, cover } = page.Attributes;
+        const blocks: KnownBlock[] = [
+          {
+            type: "header",
+            text: {
+              type: "plain_text",
+              text: title,
+              emoji: true,
+            },
+          },
+        ];
+        if (cover) {
+          blocks.push({
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: teaser,
+            },
+            accessory: {
+              type: "image",
+              image_url: cover,
+              alt_text: title,
+            },
+          });
+        } else {
+          blocks.push({
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: teaser,
+            },
+          });
+        }
+        unfurls[url] = { blocks };
+      })
+  );
+
+  await app.client.chat.unfurl({
+    token: botToken,
+    channel: event.channel,
+    ts: event.message_ts,
+    unfurls,
+  });
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("⚡️ Notion Unfurl app is running!");
+})();
