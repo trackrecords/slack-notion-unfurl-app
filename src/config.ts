ced63ea769f46da36012d3ed9c04d1b426e06608
Asato Nago <nago@tam-bourine.co.jp>
2021-07-07 12:05:45 +0900

    fix: 🐛 chore

--- src/config.ts
+++ src/config.ts
@@ -14,9 +14,10 @@ export namespace Config {
   }
   export namespace Slack {
     export const BOT_TOKEN: string = process.env.SLACK_BOT_TOKEN!;
+    // Socket Mode only
     export const APP_TOKEN: string = process.env.SLACK_APP_TOKEN!;
     export const SIGNING_SECRET: string = process.env.SLACK_SIGNING_SECRET!;
     export const PORT: number = Number(process.env.PORT) || 5000;
-    export const SOCKET_MODE: boolean = !!process.env.SOCKET_MODE || false;
+    export const SOCKET_MODE: boolean = !!APP_TOKEN;
   }
 }
